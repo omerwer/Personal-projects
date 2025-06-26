8d8fde903fdf6f0712d0c686d3056aa22176535f
Omer Wertheimer <omerw@DESKTOP-E3F2RF3>
2025-06-26 20:38:37 +0300

    without render

--- python/stocks_analysis/install_wkhtmltopdf_render.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install wkhtmltopdf static build
-echo "Installing wkhtmltopdf..."
-curl -L -o wkhtmltox.tar.xz https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.amd64.tar.xz
-tar -xf wkhtmltox.tar.xz
-mv wkhtmltox*/bin/wkhtmlto* /usr/bin/
