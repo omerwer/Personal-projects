1cfa2cf7dffb933c887e946066c833a30bef5da0
Omer Wertheimer <omerw@DESKTOP-E3F2RF3>
2025-06-29 19:07:53 +0300

    with docker for web

--- python/stocks_analysis/install_wkhtmltopdf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Installing wkhtmltopdf..."
-
-curl -L -o wkhtmltox.tar.xz https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.6/wkhtmltox-0.12.6-1.amd64.tar.xz
-tar -xf wkhtmltox.tar.xz
-sudo mv wkhtmltox*/bin/wkhtmlto* /usr/bin/
