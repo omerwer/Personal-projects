dac19abdedd04d64df5464a0331666699124048a
Omer Wertheimer <omerw@DESKTOP-E3F2RF3>
2025-08-08 19:02:42 +0300

    fix reddit

--- python/stocks-analysis/stock_scrapper.py
+++ python/stocks-analysis/stock_scrapper.py
@@ -763,7 +763,12 @@ class TickerAnalyzer:
         def get_ticker_info(self, ticker: str):
             reddit_url = f"https://www.reddit.com/search.json?q={ticker.lower()}&type=posts&sort=relevance"
 
-            headers = {"User-Agent": "Mozilla/5.0"}
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                            "(KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            }
 
             response = requests.get(reddit_url, headers=headers)
 
